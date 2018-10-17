bae2da85ade74ecd437929fb72975fffb143b98e
jstoone <jstoone@drk.sh>
2018-10-17 03:03:30 +0200

    Make class and file name fit.

--- tests/Http/Controllers/MailmanControllerTest.php
+++ tests/Http/Controllers/MailmanControllerTest.php
@@ -4,7 +4,7 @@ namespace Jstoone\Mailman\Tests\Http\Controllers;
 
 use Jstoone\Mailman\Tests\TestCase;
 
-class ToolControllerTest extends TestCase
+class MailmanControllerTest extends TestCase
 {
     /** @test */
     public function it_can_can_return_a_response()
