9802d6710c9e5eed306139464fc58d0e66c8df0c
jstoone <jstoone@drk.sh>
2018-10-17 03:02:57 +0200

    Initial mailbox skeleton.

--- resources/js/components/Tool.vue
+++ resources/js/components/Tool.vue
@@ -2,33 +2,89 @@
     <div>
         <heading class="mb-6">Mailman</heading>
 
-        <card class="bg-90 flex flex-col items-center justify-center" style="min-height: 300px">
-            <svg class="spin fill-80 mb-6" width="69" height="72" viewBox="0 0 23 24" xmlns="http://www.w3.org/2000/svg"><path d="M20.12 20.455A12.184 12.184 0 0 1 11.5 24a12.18 12.18 0 0 1-9.333-4.319c4.772 3.933 11.88 3.687 16.36-.738a7.571 7.571 0 0 0 0-10.8c-3.018-2.982-7.912-2.982-10.931 0a3.245 3.245 0 0 0 0 4.628 3.342 3.342 0 0 0 4.685 0 1.114 1.114 0 0 1 1.561 0 1.082 1.082 0 0 1 0 1.543 5.57 5.57 0 0 1-7.808 0 5.408 5.408 0 0 1 0-7.714c3.881-3.834 10.174-3.834 14.055 0a9.734 9.734 0 0 1 .03 13.855zM4.472 5.057a7.571 7.571 0 0 0 0 10.8c3.018 2.982 7.912 2.982 10.931 0a3.245 3.245 0 0 0 0-4.628 3.342 3.342 0 0 0-4.685 0 1.114 1.114 0 0 1-1.561 0 1.082 1.082 0 0 1 0-1.543 5.57 5.57 0 0 1 7.808 0 5.408 5.408 0 0 1 0 7.714c-3.881 3.834-10.174 3.834-14.055 0a9.734 9.734 0 0 1-.015-13.87C5.096 1.35 8.138 0 11.5 0c3.75 0 7.105 1.68 9.333 4.319C16.06.386 8.953.632 4.473 5.057z" fill-rule="evenodd"/></svg>
-
-            <h1 class="text-white text-4xl text-90 font-light mb-6">
-                We're in a black hole.
-            </h1>
-
-            <p class="text-white-50% text-lg">
-                This is caused by
-                <code class="ml-1 border border-80 text-sm font-mono text-white bg-black rounded px-2 py-1">
-                    jstoone/nova-mailman
-                </code>
-            </p>
-        </card>
+        <div class="flex">
+            <card class="w-1/3 mr-2">
+                <div class="border-b border-primary-10% cursor-pointer" role="alert" v-for="message in messages" :key="message.timestamp">
+                    <div class="p-4 border-l-2 border-transparent hover:border-primary" @click="setCurrentMessage(message)">
+                        <p class="flex justify-between">
+                            <span class="font-bold">{{ message.subject }}</span>
+                            <span class="text-80 text-sm">{{ formatTimestamp(message.timestamp) }}</span>
+                        </p>
+                        <p>{{ message.to }}</p>
+                    </div>
+                </div>
+            </card>
+            <card class="w-2/3" style="height: 650px;">
+                <iframe v-if="currentMessage" :src="currentMessage.content" class="w-full h-full">
+                </iframe>
+                <heading v-else class="mt-6 text-center">Select an email</heading>
+            </card>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        data() {
-            return {
-            }
+        components: {
+            // 
         },
 
-        mounted() {
-            //
+        data: () => ({
+            loaded: false,
+            currentMessage: null,
+            messages: [
+                {
+                    to: "john@example.com",
+                    subject: "Foo subject",
+                    timestamp: 1539733755,
+                    content: 'http://nova-demo.test'
+                },
+                {
+                    to: "jane@example.com",
+                    subject: "Bar subject",
+                    timestamp: 1539733200,
+                    content: 'http://import.dev-cobiro.test'
+                },
+                {
+                    to: "mailman@example.com",
+                    subject: "OMG subject",
+                    timestamp: 1529733755,
+                    content: 'http://api.dev-cobiro.test'
+                },
+            ]
+        }),
+
+        async created() {
+            // await this.getMessages();
+
+            // this.loaded = true;
+
+            // this.startPolling();
         },
+
+        methods: {
+            getMessages() {
+                //
+            },
+
+            setCurrentMessage(message) {
+                this.currentMessage = message;
+            },
+
+            formatTimestamp(timestamp) {
+                return window.moment.unix(timestamp).calendar();
+            },
+
+            startPolling() {
+                const poller = window.setInterval(() => {
+                    //
+                }, 1000);
+
+                this.$once('hook:beforeDestroy', () => {
+                    window.clearInterval(poller);
+                });
+            }
+        }
     }
 </script>
 
