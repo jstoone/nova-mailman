034de00c9fb7ffc0677871c2ab2bf74fb2354d69
jstoone <jstoone@drk.sh>
2018-11-27 01:04:22 +0100

    Split tool into individual components.

---
+++ resources/js/components/Preview.vue
@@ -0,0 +1,17 @@
+<template>
+    <card class="w-2/3" style="height: 650px;">
+        <iframe v-if="message" :src="message.link" class="w-full h-full">
+        </iframe>
+        <heading v-else class="mt-6 text-center">Select an email</heading>
+    </card>
+</template>
+
+<script>
+export default {
+    props: {
+        message: null,
+    },
+
+    methods: {},
+};
+</script>
